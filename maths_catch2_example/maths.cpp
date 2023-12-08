90040ce1eedf44d53040bf602aa7799ea0f391b6
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2023-12-08 09:49:32 +0000

    fixed is_prime error on 2

--- maths_catch2_example/maths.cpp
+++ maths_catch2_example/maths.cpp
@@ -17,7 +17,7 @@ double power(double number, int exponent)
 
 bool is_prime(int number)
 {
-  for (int i = 2; i <= number; ++i)
+  for (int i = 2; i <= number/2; ++i)
     if (number % i == 0)
       return false;
   return true;
